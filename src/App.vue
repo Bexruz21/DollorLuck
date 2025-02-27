e0ae91c7bd5f7db07fc2a42d9b109d6129d914ca
Bexruz21 <rakhmanovbexruz@gmail.com>
2025-02-27 16:52:21 +0500

    0.4.1

--- src/App.vue
+++ src/App.vue
@@ -23,7 +23,7 @@ async function checkOrRegisterUser(user) {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
-        telegram_id: user.telegram_id,
+        telegram_id: user.id,
         first_name: user.first_name,
         last_name: user.last_name || "",
         username: user.username || "",
