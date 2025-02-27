14e1ee94f70faf24c5d2627b20a68ed67313a597
Bexruz21 <rakhmanovbexruz@gmail.com>
2025-02-27 14:27:05 +0500

    0.4.0

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <script setup>
 import { onMounted, ref, provide } from 'vue';
 import { RouterView } from 'vue-router';
-import axios from 'axios';
 
 const user = ref(null);
 
@@ -15,16 +14,21 @@ onMounted(async () => {
   }
 });
 
-async function checkOrRegisterUser(telegramUser) {
-    const BASE_URL = "https://cbb0-195-158-2-216.ngrok-free.app"
-    const response = await axios.post(`${BASE_URL}/api/user/`, {
-      telegram_id: telegramUser.id,
-      username: telegramUser.username || "",
-      first_name: telegramUser.first_name,
-      last_name: telegramUser.last_name || "",
-    });
+async function checkOrRegisterUser(user) {
+    const BASE_URL = "https://b711-95-214-210-132.ngrok-free.app"
 
-    return response.data; // Возвращаем данные пользователя
+    const response = await fetch(`${BASE_URL}/api/user/`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        telegram_id: user.id,
+        username: user.username,
+        first_name: user.first_name,
+        last_name: user.last_name || "",
+        username: user.username || "",
+      }),
+    });
+    return response
 }
 provide("user", user);
 </script>
