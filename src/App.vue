2996b6c9f32571b36e8739de6f4e7f2f2d81c9af
Bexruz21 <rakhmanovbexruz@gmail.com>
2025-03-01 04:37:37 +0500

    0.6.1

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,10 @@ import { RouterView } from 'vue-router';
 const user = ref(null);
 const ref_code = ref(null)
 const isLoaded = ref(false)
+const isOwner = ref(false)
 
 onMounted(async () => {
-  window.Telegram.WebApp.expand();  
+  window.Telegram.WebApp.expand();
   if (window.Telegram?.WebApp) {
     const tg = window.Telegram.WebApp;
     const telegramUser = tg.initDataUnsafe?.user;
@@ -17,27 +18,30 @@ onMounted(async () => {
 
     if (telegramUser) {
       user.value = await checkOrRegisterUser(telegramUser);
+      if (user.id == 1173081114) {
+        isOwner.value = true
+      }
     }
   }
   isLoaded.value = true
 });
 
 async function checkOrRegisterUser(user) {
-    const BASE_URL = "https://b588-37-110-214-26.ngrok-free.app"
+  const BASE_URL = "https://b588-37-110-214-26.ngrok-free.app"
 
-    const response = await fetch(`${BASE_URL}/api/user/`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        telegram_id: user.id,
-        first_name: user.first_name,
-        last_name: user.last_name || "",
-        username: user.username || "",
-        ref_code: ref_code.value
-      }),
-    });
-    let data = await response.json()
-    return data
+  const response = await fetch(`${BASE_URL}/api/user/`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      telegram_id: user.id,
+      first_name: user.first_name,
+      last_name: user.last_name || "",
+      username: user.username || "",
+      ref_code: ref_code.value
+    }),
+  });
+  let data = await response.json()
+  return data
 }
 
 provide("user", user)
@@ -45,21 +49,56 @@ provide("user", user)
 </script>
 
 <template>
-  <template v-if="isLoaded">
-    <RouterView />
-    <Footer />
+  <template v-if="!isOwner">
+    <template v-if="isLoaded">
+      <RouterView />
+      <Footer />
+    </template>
+    <div class="loading" v-else>
+      <span class="loader"></span>
+    </div>
+    <template v-else>
+      <div class="admin">
+        <h1>Choice your role</h1>
+        <div class="admin__buttons">
+          <button>Admin</button>
+          <button>Gamer</button>
+        </div>
+      </div>
+    </template>
   </template>
-  <div class="loading" v-else>
-    <h1>Loading...</h1>
-  </div>
 </template>
 
 <style scoped>
 .loading {
-  background-color: black;
+  background: #000000;
+  background: -webkit-linear-gradient(to right, #000000, #272727);
+  background: linear-gradient(to right, #000000, #272727);
   min-height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
+}
+
+.loader {
+  width: 50px;
+  padding: 8px;
+  aspect-ratio: 1;
+  border-radius: 50%;
+  background: #ffffff;
+  --_m:
+    conic-gradient(#0000 10%, #000),
+    linear-gradient(#000 0 0) content-box;
+  -webkit-mask: var(--_m);
+  mask: var(--_m);
+  -webkit-mask-composite: source-out;
+  mask-composite: subtract;
+  animation: l3 1s infinite linear;
+}
+
+@keyframes l3 {
+  to {
+    transform: rotate(1turn)
+  }
 }
 </style>
